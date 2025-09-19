60081f24a9fe90cddb36c94d005f6450810cc06e
Angkon Kar <angkon310@gmail.com>
2025-09-19 23:48:14 +0600

    UPDATE

---
+++ Chapter 1/script.js
@@ -0,0 +1,6 @@
+// External JavaScript file
+console.log("This is from an external JS file!");
+
+// Generate random number between 1-100
+let randomNum = Math.floor(Math.random() * 100) + 1;
+console.log("Random number: " + randomNum);
